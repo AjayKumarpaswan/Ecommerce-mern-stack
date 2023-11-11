0b431a4b830c43f9485a4ed0cef13e807fc6cc8f
Ajay kumar paswan <134837380+AjayKumarpaswan@users.noreply.github.com>
2023-11-11 15:15:19 +0530

    Updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,7 +7,7 @@ import braintree from "braintree";
 import dotenv from 'dotenv'
 dotenv.config()
 //payment gateway
-var gateway = new braintree.BraintreeGateway({
+const gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
   merchantId: process.env.BRAINTREE_MERCHANT_ID,
   publicKey: process.env.BRAINTREE_PUBLIC_KEY,
